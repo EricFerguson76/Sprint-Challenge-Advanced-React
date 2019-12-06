d1cb5487c517cf46e26ca59dbcefe8f57762ef1e
Eric Ferguson <turtle0888@gmail.com>
2019-12-06 13:06:58 -0600

    Add testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,31 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
  
   render(<App />);
  
 });
+
+
+test ('Is there a toggle?', () => {
+  const { findAllByDisplayValue } = render(<App />);
+
+  findAllByDisplayValue(/toggle/i);
+})
+
+
+test ('Is there a player?', () => {
+  const { findByText } = render(<App />);
+
+  findByText(/player/i);
+})
+
+
+test ('Is there an image', () => {
+  const { findByText } = render(<App />);
+
+  findByText(/image/)
+})
+
+
